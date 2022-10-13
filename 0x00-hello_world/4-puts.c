f930e7e6577ed1a420647e2f252a568732d7fadc
Ebube Okafor <okaforebube8@gmail.com>
2022-10-13 16:37:13 -0700

    created file 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main is the entry point of the program
+ * main- is the entry point of the program
  *
- * Return by default returns o if there are no errors else it
+ * Return: by default returns o if there are no errors else it
  * returns non-zero value
  */
 int main(void)
