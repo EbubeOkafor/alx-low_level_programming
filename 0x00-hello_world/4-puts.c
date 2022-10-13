59495a3f79af3a30de00d08209a3fe444df731ab
Ebube Okafor <okaforebube8@gmail.com>
2022-10-13 16:18:27 -0700

    created 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main is the entry point of the program
+ *
+ * Return by default returns o if there are no errors else it
+ * returns non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
