f9cccdf5842d0ac7a21cb9d1091faa9997df1257
Ebube Okafor <okaforebube8@gmail.com>
2022-10-16 13:13:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char hex;
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (hex = 'A'; hex <= 'F'; hex++)
+		putchar(hex);
+	return (0);
+}
